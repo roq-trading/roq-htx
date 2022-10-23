ef9274798f3dcfa5ee7dcea9fc4c026552adc3ed
thraneh <thraneh@gmail.com>
2022-10-23 15:34:18 +0200

    chore

--- src/roq/huobi/shared.hpp
+++ src/roq/huobi/shared.hpp
@@ -5,11 +5,11 @@
 #include <chrono>
 #include <string>
 #include <utility>
+#include <vector>
 
 #include "roq/api.hpp"
 #include "roq/server.hpp"
 
-#include "roq/core/memory.hpp"
 #include "roq/core/symbols.hpp"
 #include "roq/core/timer_queue.hpp"
 
@@ -39,8 +39,8 @@ struct Shared final {
   }
 
  public:
-  core::page_aligned_vector<MBPUpdate> bids, asks, final_bids, final_asks;
-  core::page_aligned_vector<Trade> trades;
+  std::vector<MBPUpdate> bids, asks, final_bids, final_asks;
+  std::vector<Trade> trades;
 
  private:
   server::Dispatcher &dispatcher_;
