9e99b707377b75d9e8acdd9cd4ebf7148c7f61cd
thraneh <thraneh@gmail.com>
2025-11-30 10:45:39 +0100

    new json parser/dispatcher

--- src/roq/htx/json/parser.cpp
+++ src/roq/htx/json/parser.cpp
@@ -21,13 +21,26 @@ namespace roq {
 namespace htx {
 namespace json {
 
+// === CONSTANTS ===
+
+namespace {
+constexpr auto const KEY_ACTION = "action"sv;
+constexpr auto const KEY_CH = "ch"sv;
+constexpr auto const KEY_MESSAGE = "message"sv;
+constexpr auto const KEY_PING = "ping"sv;
+constexpr auto const KEY_REP = "rep"sv;
+constexpr auto const KEY_STATUS = "status"sv;
+constexpr auto const KEY_SUBBED = "subbed"sv;
+}  // namespace
+
 // === HELPERS ===
 
 namespace {
 template <typename T>
-void dispatch_helper(auto &handler, auto &message, auto &buffer_stack, auto &trace_info) {
+auto dispatch_helper(auto &handler, auto &message, auto &buffer_stack, auto &trace_info) {
   T obj{message, buffer_stack};
   create_trace_and_dispatch(handler, trace_info, obj);
+  return true;
 }
 }  // namespace
 
@@ -39,211 +52,108 @@ bool Parser::dispatch(
     core::json::BufferStack &buffer_stack,
     TraceInfo const &trace_info,
     bool allow_unknown_event_types) {
-  auto stop = false;
   auto result = false;
   auto helper = [&](auto &key, auto &value) {
-    if (result || stop) {  // XXX FIXME TODO can we stop early?
-      return;
-    }
-    auto k = utils::hash::FNV::compute(key);
-    switch (k) {
-      case utils::hash::FNV::compute("action"sv): {
-        Action action{std::get<std::string_view>(value)};
+    auto key_2 = utils::hash::FNV::compute(key);
+    switch (key_2) {
+      case utils::hash::FNV::compute(KEY_ACTION): {
+        Action action{value};
         switch (action) {
           using enum Action::type_t;
           case UNDEFINED_INTERNAL:
-            break;  // XXX stop?
+            log::fatal("Unexpected"sv);
           case UNKNOWN_INTERNAL:
-            if (allow_unknown_event_types) {
-              stop = true;
-            }
-            break;
+            return true;
           case SUB:
-            result = true;
-            dispatch_helper<json::Sub>(handler, message, buffer_stack, trace_info);
+            result = dispatch_helper<json::Sub>(handler, message, buffer_stack, trace_info);
             break;
           case REQ:
-            result = true;
-            dispatch_helper<json::Req>(handler, message, buffer_stack, trace_info);
+            result = dispatch_helper<json::Req>(handler, message, buffer_stack, trace_info);
             break;
           case PING:
-            result = true;
-            dispatch_helper<json::Ping>(handler, message, buffer_stack, trace_info);
+            result = dispatch_helper<json::Ping>(handler, message, buffer_stack, trace_info);
             break;
           case PONG:
-            stop = true;
-            // dispatch_helper<json::Pong>(handler, message, buffer_stack, trace_info);
+            // result = dispatch_helper<json::Pong>(handler, message, buffer_stack, trace_info);
+            return true;
             break;
           case PUSH:
-            stop = true;
-            // dispatch_helper<json::Push>(handler, message, buffer_stack, trace_info);
+            // result = dispatch_helper<json::Push>(handler, message, buffer_stack, trace_info);
+            return true;
             break;
         }
         break;
       }
-      case utils::hash::FNV::compute("ch"sv): {
-        Topic topic{extract_topic(std::get<std::string_view>(value))};
+      case utils::hash::FNV::compute(KEY_CH): {
+        Topic topic{value};
         switch (topic) {
           using enum Topic::type_t;
           case UNDEFINED_INTERNAL:
-            break;
+            log::fatal("Unexpected"sv);
           case UNKNOWN_INTERNAL:
-            if (allow_unknown_event_types) {
-              stop = true;
-            }
-            break;
+            return true;
           case BBO:
-            result = true;
-            dispatch_helper<json::BBO>(handler, message, buffer_stack, trace_info);
+            result = dispatch_helper<json::BBO>(handler, message, buffer_stack, trace_info);
             break;
           case TRADE:
-            result = true;
-            dispatch_helper<Trade>(handler, message, buffer_stack, trace_info);
+            result = dispatch_helper<Trade>(handler, message, buffer_stack, trace_info);
             break;
           case DETAIL:
-            result = true;
-            dispatch_helper<Detail>(handler, message, buffer_stack, trace_info);
+            result = dispatch_helper<Detail>(handler, message, buffer_stack, trace_info);
             break;
           case TICKER:
-            result = true;
-            dispatch_helper<Ticker>(handler, message, buffer_stack, trace_info);
+            result = dispatch_helper<Ticker>(handler, message, buffer_stack, trace_info);
             break;
           case MBP:
-            result = true;
-            dispatch_helper<json::MBP>(handler, message, buffer_stack, trace_info);
+            result = dispatch_helper<json::MBP>(handler, message, buffer_stack, trace_info);
             break;
           case AUTH:
-            // result = true;
-            // dispatch_helper<json::Auth>(handler, message, buffer_stack, trace_info);
+            // result = dispatch_helper<json::Auth>(handler, message, buffer_stack, trace_info);
+            return true;
             break;
         }
         break;
       }
-      case utils::hash::FNV::compute("message"sv): {
-        result = true;
-        dispatch_helper<Error2>(handler, message, buffer_stack, trace_info);
+      case utils::hash::FNV::compute(KEY_MESSAGE): {
+        result = dispatch_helper<Error2>(handler, message, buffer_stack, trace_info);
         break;
       }
-      case utils::hash::FNV::compute("ping"sv): {
-        result = true;
-        dispatch_helper<Ping2>(handler, message, buffer_stack, trace_info);
+      case utils::hash::FNV::compute(KEY_PING): {
+        result = dispatch_helper<Ping2>(handler, message, buffer_stack, trace_info);
         break;
       }
-      case utils::hash::FNV::compute("rep"sv): {
+      case utils::hash::FNV::compute(KEY_REP): {
         Topic topic{extract_topic(std::get<std::string_view>(value))};
         if (topic == Topic::MBP) {
-          result = true;
-          dispatch_helper<MBPSnapshot>(handler, message, buffer_stack, trace_info);
+          result = dispatch_helper<MBPSnapshot>(handler, message, buffer_stack, trace_info);
         }
         break;
       }
-      case utils::hash::FNV::compute("status"sv): {
-        Status status{std::get<std::string_view>(value)};
+      case utils::hash::FNV::compute(KEY_STATUS): {
+        Status status{value};
         switch (status) {
           using enum Status::type_t;
           case UNDEFINED_INTERNAL:
-            break;  // XXX stop?
+            log::fatal("Unexpected"sv);
           case UNKNOWN_INTERNAL:
-            if (allow_unknown_event_types) {
-              stop = true;
-            }
-            break;
+            return true;
           case OK:
             break;  // note! continue
           case ERROR:
-            result = true;
-            dispatch_helper<json::Error>(handler, message, buffer_stack, trace_info);
+            result = dispatch_helper<json::Error>(handler, message, buffer_stack, trace_info);
+            break;
         }
         break;
       }
-      case utils::hash::FNV::compute("subbed"sv): {
-        result = true;
-        dispatch_helper<Subbed>(handler, message, buffer_stack, trace_info);
+      case utils::hash::FNV::compute(KEY_SUBBED): {
+        result = dispatch_helper<Subbed>(handler, message, buffer_stack, trace_info);
         break;
       }
     }
-    /*
-    if (!frame.ping.count()) {
-      switch (frame.status) {
-        using enum Status::type_t;
-        case UNDEFINED_INTERNAL: {
-          Topic topic{extract_topic(frame.ch)};
-          switch (topic) {
-            using enum Topic::type_t;
-            case UNDEFINED_INTERNAL:
-              break;
-            case UNKNOWN_INTERNAL:
-              if (allow_unknown_event_types) {
-                return false;
-              }
-              break;
-            case BBO:
-              dispatch_helper<json::BBO>(handler, message, buffer_stack, trace_info);
-              return true;
-            case TRADE:
-              dispatch_helper<Trade>(handler, message, buffer_stack, trace_info);
-              return true;
-            case DETAIL:
-              dispatch_helper<Detail>(handler, message, buffer_stack, trace_info);
-              return true;
-            case TICKER:
-              dispatch_helper<Ticker>(handler, message, buffer_stack, trace_info);
-              return true;
-            case MBP:
-              dispatch_helper<json::MBP>(handler, message, buffer_stack, trace_info);
-              return true;
-          }
-          break;
-        }
-        case UNKNOWN_INTERNAL:
-          if (allow_unknown_event_types) {
-            return false;
-          }
-          break;
-        case OK:
-          if (!std::empty(frame.subbed)) {
-            auto subbed = Subbed{
-                .id = frame.id,
-                .subbed = frame.subbed,
-                .ts = frame.ts,
-                .status = frame.status,
-            };
-            create_trace_and_dispatch(handler, trace_info, subbed);
-            return true;
-          } else {
-            if (!std::empty(frame.rep)) {
-              Topic topic{extract_topic(frame.rep)};
-              if (topic == Topic::MBP) {
-                dispatch_helper<MBPSnapshot>(handler, message, buffer_stack, trace_info);
-                return true;
-              }
-            }
-            log::fatal("DEBUG {}"sv, message);  // ???
-          }
-          break;
-        case ERROR:
-          auto error = Error{
-              .id = frame.id,
-              .err_code = frame.err_code,
-              .err_msg = frame.err_msg,
-              .ts = frame.ts,
-          };
-          create_trace_and_dispatch(handler, trace_info, error);
-          return true;
-      }
-    } else {
-      auto ping = Ping{
-          .timestamp = frame.ping,
-      };
-      create_trace_and_dispatch(handler, trace_info, ping);
-      return true;
-    }
-    */
+    return result;
   };
-  core::json::Parser parser{message};
-  auto value = parser.root();
-  std::get<core::json::Object>(value).dispatch(helper);
-  if (result || stop) {
+  core::json::Parser::dispatch<core::json::Object>(helper, message);
+  if (result || allow_unknown_event_types) {
     return result;
   }
   log::fatal(R"(Unexpected: message="{}")"sv, message);
