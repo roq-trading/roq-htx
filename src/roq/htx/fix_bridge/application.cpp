fcfad513c78fd951bd1d92f67f1d99119df2b64e
thraneh <thraneh@gmail.com>
2026-06-28 02:52:47 +0000

    moving fix_bridge into roq-server

---
+++ src/roq/htx/fix_bridge/application.cpp
@@ -0,0 +1,33 @@
+/* Copyright (c) 2017-2026, Hans Erik Thrane */
+
+#include "roq/htx/fix_bridge/application.hpp"
+
+#include "roq/logging.hpp"
+
+#include "roq/server/fix_bridge/controller.hpp"
+
+#include "roq/htx/gateway/controller.hpp"
+
+#include "roq/htx/fix_bridge/config.hpp"
+#include "roq/htx/fix_bridge/settings.hpp"
+
+using namespace std::literals;
+
+namespace roq {
+namespace htx {
+namespace fix_bridge {
+
+// === IMPLEMENTATION ===
+
+int Application::main(args::Parser const &args) {
+  Settings settings{args};
+  Config config{settings};
+  log::warn("config={}"sv, config);
+  auto context = server::create_io_context(settings);
+  server::fix_bridge::Controller2<gateway::Controller>{settings, config, *context, "trader"sv}.dispatch();
+  return EXIT_SUCCESS;
+}
+
+}  // namespace fix_bridge
+}  // namespace htx
+}  // namespace roq
---
+++ src/roq/htx/fix_bridge/config.hpp
@@ -0,0 +1,15 @@
+/* Copyright (c) 2017-2026, Hans Erik Thrane */
+
+#pragma once
+
+#include "roq/htx/gateway/config.hpp"
+
+namespace roq {
+namespace htx {
+namespace fix_bridge {
+
+using Config = gateway::Config;
+
+}  // namespace fix_bridge
+}  // namespace htx
+}  // namespace roq
