9250416b6a9192d243de653f8eb1324ac7308512
thraneh <thraneh@gmail.com>
2026-09-15 13:43:27 +0000

    Adding ReferenceData.market_segment and MarketSegmentStatus #636

--- src/roq/htx/gateway/rest.cpp
+++ src/roq/htx/gateway/rest.cpp
@@ -373,6 +373,7 @@ void Rest::operator()(Trace<protocol::json::Symbols> const &event) {
         .description = {},
         .security_type = {},
         .external_security_id = {},
+        .market_segment = {},
         .cfi_code = {},
         .base_currency = item.base_currency,
         .quote_currency = item.quote_currency,
